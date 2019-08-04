3f2fee64a0cf37cddab46f65fa2b0d24b08f0c6a
“Januarylovelulu” <907971060@qq.com>
2019-08-04 11:42:38 +0800

    January

--- home/assets/js/startMove.js
+++ home/assets/js/startMove.js
@@ -28,48 +28,6 @@ function startMove(obj, name, number, number2 = 0) {
 	}, 10)
 }
 
-/*
-	运动框架
-	@elem 待运动的对象
-	@targetJson 对象运动的重点位置
-	@duration 动画的持续时间
-*/
-function animate(elem, targetJson, duration) {
-
-	var interval = 20; //定义动画运动的间隔时间20毫秒
-	var objInitJson = {}; //用来存储每一个属性的默认初始值
-	for (var attr in targetJson) {
-		objInitJson[attr] = parseInt(getStyle(elem, attr));
-	}
-	var totalFrames = duration / interval; //动画运动的总帧数
-
-	var totalChange = {}; //用来存储每一个属性的总的变化量
-	for (var attr in targetJson) {
-		totalChange[attr] = targetJson[attr] - objInitJson[attr];
-	}
-	var objStepJson = {}; //每一个属性在每一帧的变化量
-	for (var attr in targetJson) {
-		objStepJson[attr] = parseFloat(totalChange[attr] / totalFrames);
-	}
-
-
-	var runCounts = 1;
-	var timer = setInterval(function() {
-		runCounts++;
-		for (var attr in objStepJson) {
-			objInitJson[attr] += objStepJson[attr];
-			elem.style[attr] = objInitJson[attr] + "px";
-		}
-		if (runCounts > totalFrames) {
-			for (var attr in objStepJson) {
-				elem.style[attr] = targetJson[attr] + "px";
-			}
-			clearInterval(timer);
-		}
-
-	}, interval);
-}
-
 function getStyle(obj, name) {
 	if (obj.currentStyle)
 		return obj.currentStyle[name];
